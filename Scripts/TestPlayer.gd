64a039afb1f436fd55bc09961ba5afe1e5953d0d
Patotatoman27 <patotatomanyt@gmail.com>
2025-05-15 19:48:08 -0600

    P: Inputs P1 y Mejora Salto/Movimiento

--- Scripts/TestPlayer.gd
+++ Scripts/TestPlayer.gd
@@ -1,10 +1,11 @@
 extends CharacterBody2D
 
-
-const SPEED = 500.0
+const SPEED = 400.0
 @export var JUMP_VELOCITY = -400.0;
 const QUEQUE_JUMP_FRAMES = 10;
 const GRAVITY = Vector2(0, 1580);
+const ACCELERATION = 5000
+const FRICTION = 9999
 
 var quequedJump : bool;
 var quequedJumpFrames : int;
@@ -25,7 +26,7 @@ func _physics_process(delta: float) -> void:
 		additionalGravity = 0;
 
 	# Salto. Al darle al boton de salto guardalo hasta que sea usable por x frames.
-	if Input.is_action_just_pressed("ui_accept"):
+	if Input.is_action_just_pressed("P1_Jump"):
 		quequedJump = true;
 		quequedJumpFrames = QUEQUE_JUMP_FRAMES;
 	if quequedJump:
@@ -36,13 +37,15 @@ func _physics_process(delta: float) -> void:
 			quequedJumpFrames -= 1;
 			if quequedJumpFrames <= 0:
 				quequedJump = false;
-			
 
 	#Movimiento horizontal
-	var direction := Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+	var direction := Input.get_axis("P1_Left", "P1_Right")
 
+	if direction != 0:
+		velocity.x += direction * ACCELERATION * delta
+		if abs(velocity.x) > SPEED:
+			velocity.x = sign(velocity.x) * SPEED
+	else:
+		velocity.x = 0
+	
 	move_and_slide()
