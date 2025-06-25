4ccf1e8a74dbeb4d2d89e494086ebcbfdff2da64
Patotatoman27 <patotatomanyt@gmail.com>
2025-06-25 16:25:45 -0600

    P: Reestructura de Neumann (ahora usa Character) y eliminados Greumann y Yeumann

---
+++ Scripts/characters/neumann.gd
@@ -0,0 +1,35 @@
+extends Character
+
+func _ready() -> void:
+	super._ready();
+
+func _physics_process(delta: float) -> void:
+#State management
+	match state:
+		States.cantMove:
+			stateCantMove();
+		States.cantMoveFell:
+			stateCantMoveFell(delta);
+		States.hitstun:
+			stateHitstun(delta);
+		States.idle:
+			stateIdle();
+		States.walk:
+			move = Moveset.testWalkKick
+			stateWalk(delta);
+		States.dashStart:
+			stateDashStart();
+		States.dash:
+			stateDash(delta);
+		States.dashHold:
+			stateDashHold();
+		States.jump:
+			stateJump(delta);
+		States.doublejump:
+			move = Moveset.testJumpKick
+			stateDoubleJump(delta);
+		States.fall:
+			stateFall(delta);
+		States.realfall:
+			stateRealFall(delta);
+	applyMovement(delta);
--- Scripts/mainTestFight.gd
+++ Scripts/mainTestFight.gd
@@ -9,7 +9,7 @@ var MTS; #Aqui va el MainTestStage
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	setUpFight("Redeumann", "Blueumann", "null");
+	setUpFight("Neumann", "Neumann", "MainTheme");
 
 func setUpFight(char1 : String, char2 : String, music : String):
 	roundNumber = 1;
